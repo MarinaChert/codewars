1c166f3748567ca1f535caa2d707459a979d4de3
MarinaChert <mar.chertkova@mail.ru>
2024-08-30 16:44:52 +0300

    reversed-strings.js_v1

---
+++ java-script-8kyu/reversed-strings.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// Solution
+function solution(str){
+    return str.split("").reverse().join("");
+  }
+  console.log("Result:",solution("world"));
+  console.log("Result:",solution("word"));
