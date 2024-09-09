ed61678e72fadaa9be299461504c37582d00b04d
MarinaChert <mar.chertkova@mail.ru>
2024-09-09 18:52:53 +0300

    Check if a number is a part of an arbitrary fibonacci sequence

---
+++ java-script-7kyu/arbitrary-fibonacci-sequence.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// You will be given a function, isFibo, which will take three arguments: a, b and f.
+// a is the first term of a Fibonacci sequence, b is the second term of this sequence ( it will always be bigger than or equal to a and they'll be both always positive, so don't worry about pesky input checking ) and f is a number which you'll need to check whether or not is a part of this Fibonacci sequence.
+
+// Solution
+function isFibo(a, b, f) {
+    while (a < f) {
+        [a, b] = [b, a + b]
+    }
+    return a === f;
+}
+
+console.log("Result:", isFibo(1, 3, 1));
